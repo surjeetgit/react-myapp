25f3ee08b3f652dfee86ec856469d83fe27c2ab7
Surjeet Singh <singhsurjeet2005@gmail.com>
2026-03-18 13:28:52 +0530

    created new component

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,11 +1,13 @@
 import reactLogo from "./assets/react.svg"
 import reactIcon from "/favicon.svg"
+import Map from "./Map.jsx";
 function App() {
   return (
     <>
       <img src={reactLogo} width="50px" />
       <h1>My First App..</h1>
       <img src={reactIcon} width="50px" />
+      <Map />
     </>
   );
 }
---
+++ first-app/src/Map.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Map() {
+    const user = "Surjeet"
+    return <div>Hello {user}!!</div>;
+}
+
+export default Map

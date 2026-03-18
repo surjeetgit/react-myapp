ac312efeeef94fad70a4149074ca145262a076aa
Surjeet Singh <singhsurjeet2005@gmail.com>
2026-03-18 13:21:13 +0530

    use of images

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,10 +1,13 @@
-
+import reactLogo from "./assets/react.svg"
+import reactIcon from "/favicon.svg"
 function App() {
   return (
     <>
+      <img src={reactLogo} width="50px" />
       <h1>My First App..</h1>
+      <img src={reactIcon} width="50px" />
     </>
-  )
+  );
 }
 
 export default App
